9141462fabbda887659d8b17067c908f228bed4e
Masud <wubetumasud@gmail.com>
2026-03-15 11:41:18 +0300

    Tpescript practice

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let sales = 123_456_789;
+let course = 'Typescript';
+let is_published = true;
+let age;
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 0] = "Small";
+    Size[Size["Medium"] = 1] = "Medium";
+    Size[Size["Large"] = 2] = "Large";
+})(Size || (Size = {}));
+;
+let mySize = Size.Medium;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+
+
+const enum Size { Small, Medium, Large };
+
+let mySize: Size = Size.Medium
+console.log(mySize);
+
+function calculateTax(income: number, taxYear: number = 2022): number {
+    if( taxYear < 2022) 
+        return income * 1.2
+    return income * 1.3
+}
+
+calculateTax(10_000);
+
+let employee: {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+} = { 
+    id: 1,
+    name: 'Masud Wubetu', 
+    retire: (date: Date) => {
+        console.log(date)
+    }
+ }
