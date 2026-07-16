768cefdd126896230d78aec5cc1bdef19142b364
Your Name <wubetumasud@gmail.com>
2026-07-16 04:50:15 +0300

    Access modifiers

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,34 @@
 class Account {
     readonly id: number;
     owner: string;
-    balance: number;
+    private _balance: number;
     nickname?: string
 
 
     constructor(id:number, owner: string, balance: number) {
         this.id = id,
         this.owner = owner,
-        this.balance = balance
+        this._balance = balance
     }
 
     deposit(amount: number) {
         if ( amount <= 0) 
             throw new Error('Invalid amount')
-        this.balance += amount
+        this._balance += amount
+    }
+
+    private calculateTax() {
+
+    }
+    getBalance(): number {
+        return this._balance
     }
 }
 
 let account = new Account(1, 'Masud', 0);
 
 account.deposit(100);
-console.log(account.balance);
+console.log(account.getBalance());
+account.
 
 
