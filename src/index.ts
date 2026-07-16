c14680bbf8a5463fabce3f9bd73b7fdb1a9e1734
Your Name <wubetumasud@gmail.com>
2026-07-16 09:33:14 +0300

    Parameter properties

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 class Account {
-    readonly id: number;
-    owner: string;
-    private _balance: number;
     nickname?: string
 
 
-    constructor(id:number, owner: string, balance: number) {
-        this.id = id,
-        this.owner = owner,
-        this._balance = balance
+    constructor(
+        public readonly id:number, 
+        public owner: string,
+        private _balance: number) {
     }
 
     deposit(amount: number) {
@@ -29,6 +26,7 @@ let account = new Account(1, 'Masud', 0);
 
 account.deposit(100);
 console.log(account.getBalance());
-account.
+
+
 
 
