fe15071f78fde2007e84c3aae6a78c2c04902ace
Your Name <wubetumasud@gmail.com>
2026-07-18 17:13:42 +0300

    extending generic class by Pass on, restrict or Fix the generic class

--- src/index.ts
+++ src/index.ts
@@ -214,4 +214,18 @@ class CompressibleStore<T> extends Store<T> {
 }
 
 let store = new CompressibleStore<Product>();
-store.add({name: 'abebe', price: 12})
+store.add({name: 'abebe', price: 12});
+
+// Restrict the generic type parameter
+class SearchableStore<T extends { name: string }> extends Store<T> {
+    find(name: string): T | undefined {
+        return this._objects.find(obj => obj.name === name);
+    }
+}
+
+// Fix the generic type parameter
+class ProductStore extends Store<Product> {
+    filterByCategory(category: string): Product[] {
+        return [];
+    }
+}
