78cea2a89286a0eb7165cb281e32b280dc4ac569
Your Name <wubetumasud@gmail.com>
2026-07-23 09:54:32 +0300

    import and export module

--- src/index.ts
+++ src/index.ts
@@ -330,32 +330,40 @@
 // let person =  new Person("masud", "wubetu");
 // console.log(person.fullName);
 
-function MinLength(length: number) {
-    return (target: any, propertyName: string) => {
-        let value: string;
+// function MinLength(length: number) {
+//     return (target: any, propertyName: string) => {
+//         let value: string;
 
-        const descriptor: PropertyDescriptor = {
-            get() { return value; },
-            set(newValue: string) {
-                if (newValue.length < length) 
-                    throw new Error(`${propertyName} should be at least ${length} long.`);
-                value = newValue;
-            }
-        };
+//         const descriptor: PropertyDescriptor = {
+//             get() { return value; },
+//             set(newValue: string) {
+//                 if (newValue.length < length) 
+//                     throw new Error(`${propertyName} should be at least ${length} long.`);
+//                 value = newValue;
+//             }
+//         };
 
-        Object.defineProperty(target, propertyName, descriptor);
-    }
-}
+//         Object.defineProperty(target, propertyName, descriptor);
+//     }
+// }
 
 
-class User {
-    @MinLength(4)
-    password: string;
+// class User {
+//     @MinLength(4)
+//     password: string;
 
-    constructor(password: string) {
-        this.password = password;
-    }
-}
+//     constructor(password: string) {
+//         this.password = password;
+//     }
+// }
 
-let user = new User('12');
-console.log(user.password);
+// let user = new User('12');
+// console.log(user.password);
+
+import { Circle, Square } from './shape'
+
+
+let circle = new Circle(1);
+let square = new Square(2);
+
+console.log(square);
